17ba6cab1fe84a1cf578736da64acd8b329b83d1
Malivil <mlebson@outlook.com>
2020-10-15 15:47:21 -0400

    Moved distribution scripts to their own repo

--- CreateAddonGMA.bat
+++
@@ -1,6 +0,0 @@
-"C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmad.exe" create -folder %1
-echo Removing old file
-del TTT-Zombie-Perk-Bottles.gma
-echo Renaming file
-ren %1.gma TTT-Zombie-Perk-Bottles.gma
-pause
--- UpdatePerkBottles.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set /p changes="Enter Changes: "
-"C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe"  update -addon "TTT-Zombie-Perk-Bottles.gma" -id "2170163632" -changes "%changes%"
-pause
